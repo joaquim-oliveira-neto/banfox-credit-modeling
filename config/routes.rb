29a6b17195701c5e079f0de3efc65e484a01f12c
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-02-08 09:52:28 -0200

    took out the subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  constraints(Subdomain) do
-    devise_for :users
-    root to: 'pages#home'
-    resources :report_requests, only: [:new, :create, :index, :show]
-  end
+  devise_for :users
+  root to: 'pages#home'
+  resources :report_requests, only: [:new, :create, :index, :show]
 end
