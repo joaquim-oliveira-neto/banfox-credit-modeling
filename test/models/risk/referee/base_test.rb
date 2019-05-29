a44b2482add6ecde2f6aa4d093722836970549d8
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-05-29 18:53:23 -0300

    refactoring assert to assert_equal

--- test/models/risk/referee/base_test.rb
+++ test/models/risk/referee/base_test.rb
@@ -1,7 +1,9 @@
+require 'test_helper'
+
 class Risk::Referee::BaseTest < ActiveSupport::TestCase
   #Mocked class
   class SpecificReferee < Risk::Referee::Base
-    require_external_service :serasa_relato
+    require_external_service Risk::Service::Serasa
     configure_identifier code: 'Risk 0001' ,
                          title: 'Very Specific Referee',
                          description: 'Very specific description'
@@ -14,7 +16,7 @@ class Risk::Referee::BaseTest < ActiveSupport::TestCase
   end
 
   test 'configure required documents' do
-    assert SpecificReferee.required_external_services.count == 1
+    assert_equal SpecificReferee.required_external_services.count, 1
   end
 
   test 'search required documents before executing the algorithm' do
@@ -23,32 +25,32 @@ class Risk::Referee::BaseTest < ActiveSupport::TestCase
   end
 
   test 'configure Risk title' do
-    assert SpecificReferee.title == 'Very Specific Referee'
+    assert_equal SpecificReferee.title, 'Very Specific Referee'
   end
 
   test 'configure Risk code' do
-    assert SpecificReferee.code == 'Risk 0001'
+    assert_equal SpecificReferee.code, 'Risk 0001'
   end
 
   test 'configure Risk description' do
-    assert SpecificReferee.description == 'Very specific description'
+    assert_equal SpecificReferee.description, 'Very specific description'
   end
 
   test 'generate a KeyRiskIdentifier with a red flag' do
     algorithm = SpecificReferee.new
     kri = algorithm.red!
-    assert kri.flag == Risk::Referee::Base::RED_FLAG
+    assert_equal kri.flag, Risk::Referee::Base::RED_FLAG
   end
 
   test 'generate a KeyRiskIdentifier with a yellow flag' do
     algorithm = SpecificReferee.new
     kri = algorithm.yellow!
-    assert kri.flag == Risk::Referee::Base::YELLOW_FLAG
+    assert_equal kri.flag, Risk::Referee::Base::YELLOW_FLAG
   end
 
   test 'generate a KeyRiskIdentifier with a green flag' do
     algorithm = SpecificReferee.new
     kri = algorithm.green!
-    assert kri.flag == Risk::Referee::Base::GREEN_FLAG
+    assert_equal kri.flag, Risk::Referee::Base::GREEN_FLAG
   end
 end
